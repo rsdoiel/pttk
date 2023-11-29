411fa2f47c7a3474787877f975ddfb9dda442efc
R. S. Doiel <rsdoiel@gmail.com>
2023-11-29 11:48:05 -0800

    Quick Save

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-11-29"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "e1653fe"
+    ReleaseHash = "8454b30"
 
     LicenseText = `
 pttk, a plain text toolkit
